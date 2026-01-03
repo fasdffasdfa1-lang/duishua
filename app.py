405f3528cb70acb4822786b4e63d1bd033442aae
fasdffasdfa1-lang <fasdffasdfa1@gmail.com>
2026-01-03 13:40:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4129,7 +4129,7 @@ def main():
             max_ratio = st.slider("最大金额差距倍数", 
                                  min_value=2, 
                                  max_value=20, 
-                                 value=10, 
+                                 value=2, 
                                  step=1,
                                  help="组内最大金额与最小金额的允许倍数（例如：10表示10倍差距）")
         
